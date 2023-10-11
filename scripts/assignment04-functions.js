332f94f5c21a3e54d31a2b92d0eb69088a13db52
ethannewton500 <109337928+ethannewton500@users.noreply.github.com>
2023-10-11 14:12:52 -0700

    completed essential css and styled documents

--- scripts/assignment04-functions.js
+++ scripts/assignment04-functions.js
@@ -1,82 +1,65 @@
-function listImages(imageArray)
-{
-    let imageList = "<ul>";
+function listImages(imageArray) {
+	let imageList = "<ul>";
 
-    if(!Array.isArray(imageArray))
-    {
-        return `*** Error: you must provide an array. \"${imageArray}\" is not an array ***`;
-    }
+	if (!Array.isArray(imageArray)) {
+		return `*** Error: you must provide an array. \"${imageArray}\" is not an array ***`;
+	}
 
-    imageArray.forEach(image => {
-        if(typeof image !== "string")
-        {
-            console.error(`*** Error: ${image} was not a string, it will not be added ***`);
-        }
-        else
-        {
-            imageList += `<li><img src="${pathToImages}${image}" alt="${pathToImages, image}" class="listImage"></li>`;
-        }
-    });
+	imageArray.forEach((image) => {
+		if (typeof image !== "string") {
+			console.error(`*** Error: ${image} was not a string, it will not be added ***`);
+		} else {
+			imageList += `<li><img src="${pathToImages}${image}" alt="${(pathToImages, image)}" class="listImage"></li>`;
+		}
+	});
 
-    imageList += "</ul>";
-    return imageList;
+	imageList += "</ul>";
+	return imageList;
 }
 
-function listHyperLinks(linkArray)
-{
-    let linkList = "<ul>";
+function listHyperLinks(linkArray) {
+	let linkList = "<ul>";
 
-    if(!Array.isArray(linkArray))
-    {
-        return `*** Error: you must provide an array. \"${linkArray}\" is not an array ***`;
-    }
+	if (!Array.isArray(linkArray)) {
+		return `*** Error: you must provide an array. \"${linkArray}\" is not an array ***`;
+	}
 
-    linkArray.forEach(link => {
-        if(typeof link !== "string")
-        {
-            console.error(`*** Error: ${link} was not a string, it will not be added ***`);
-        }
-        else
-        {
-            linkList += `<li><a href="${link}">${link}</a></li>`;
-        }
-    })
+	linkArray.forEach((link) => {
+		if (typeof link !== "string") {
+			console.error(`*** Error: ${link} was not a string, it will not be added ***`);
+		} else {
+			linkList += `<li><a href="${link}">${link}</a></li>`;
+		}
+	});
 
-    linkList += "</ul>";
-    return linkList;
+	linkList += "</ul>";
+	return linkList;
 }
 
-function listNames(namesArray, listType = "ul")
-{
-    if (!Array.isArray(namesArray)) 
-    {
-        return `*** Error: you must provide an array. \"${namesArray}\" is not an array ***`;
-    }
+function listNames(namesArray, listType = "ul") {
+	if (!Array.isArray(namesArray)) {
+		return `*** Error: you must provide an array. \"${namesArray}\" is not an array ***`;
+	}
 
-    if (listType !== 'ul' && listType !== 'ol') 
-    {
-        return `*** Error: The second parameter ${listType} must be either "ul" or "ol" ***`;
-    }
+	if (listType !== "ul" && listType !== "ol") {
+		return `*** Error: The second parameter ${listType} must be either "ul" or "ol" ***`;
+	}
 
-    if (namesArray.length <= 1)
-    {
-        return "*** Error: Array must contain at least 2 items ***"
-    }
+	if (namesArray.length <= 1) {
+		return "*** Error: Array must contain at least 2 items ***";
+	}
 
-    let listOfNames = `<${listType}>`;
+	let listOfNames = `<${listType}>`;
 
-    namesArray.forEach(name => {
-        if (typeof name !== "string") 
-        {
-            console.error(`*** Error: "${name}" was not a string, it will not be added ***`);
-        }
-        else
-        {
-            listOfNames += `<li>${name}</li>`;
-        }
-    });
+	namesArray.forEach((name) => {
+		if (typeof name !== "string") {
+			console.error(`*** Error: "${name}" was not a string, it will not be added ***`);
+		} else {
+			listOfNames += `<li>${name}</li>`;
+		}
+	});
 
-    listOfNames += `</${listType}>`;
+	listOfNames += `</${listType}>`;
 
-    return listOfNames;
+	return listOfNames;
 }
--- scripts/assignment04.js
+++ scripts/assignment04.js
@@ -1,36 +1,33 @@
 const currentTime = new Date();
 const currentHour = currentTime.getHours();
 
-const timeMessageElement = document.getElementById('timeMessage');
-const timeImageElement = document.getElementById('timeImage');
+const timeMessageElement = document.getElementById("timeMessage");
+const timeImageElement = document.getElementById("timeImage");
 
-const wrongImageArray = document.getElementById('imageWrongArrayArgument');
-const alsoWrongImageArray = document.getElementById('alsoImageWrongArrayArgument');
-const wrongImageNumbers = document.getElementById('imageNumbers');
-const wrongImageBooleans = document.getElementById('imageBooleans');
-const validImage = document.getElementById('imageValid');
-const junkImage = document.getElementById('imageJunk');
+const wrongImageArray = document.getElementById("imageWrongArrayArgument");
+const alsoWrongImageArray = document.getElementById("alsoImageWrongArrayArgument");
+const wrongImageNumbers = document.getElementById("imageNumbers");
+const wrongImageBooleans = document.getElementById("imageBooleans");
+const validImage = document.getElementById("imageValid");
+const junkImage = document.getElementById("imageJunk");
 
-const booleanLinks = document.getElementById('linksBooleans');
-const numberNotArray = document.getElementById('numberNotArray');
-const validUrlArray = document.getElementById('validUrlArray');
-const validUrlArrayJunk = document.getElementById('validUrlArrayJunk');
+const booleanLinks = document.getElementById("linksBooleans");
+const numberNotArray = document.getElementById("numberNotArray");
+const validUrlArray = document.getElementById("validUrlArray");
+const validUrlArrayJunk = document.getElementById("validUrlArrayJunk");
 
-const notNameArray = document.getElementById('notNameArray');
-const smallArray = document.getElementById('smallArray');
-const invalidSecondParamater = document.getElementById('invalidSecondParamater');
-const validUlArray = document.getElementById('validUlArray');
-const validOlArray = document.getElementById('validOlArray');
+const notNameArray = document.getElementById("notNameArray");
+const smallArray = document.getElementById("smallArray");
+const invalidSecondParamater = document.getElementById("invalidSecondParamater");
+const validUlArray = document.getElementById("validUlArray");
+const validOlArray = document.getElementById("validOlArray");
 
-if (currentHour < 12)
-{
-    timeMessageElement.innerHTML = "Good morning its a beautiful day!";
-    timeImageElement.src = 'images/sun.png';
-}
-else
-{
-    timeMessageElement.innerHTML = "Good evening its a dark spooky night!";
-    timeImageElement.src = 'images/moon.png';
+if (currentHour < 12) {
+	timeMessageElement.innerHTML = "Good morning its a beautiful day!";
+	timeImageElement.src = "images/sun.png";
+} else {
+	timeMessageElement.innerHTML = "Good evening its a dark spooky night!";
+	timeImageElement.src = "images/moon.png";
 }
 
 wrongImageArray.innerHTML = listImages(notAnArrayOfImages);
